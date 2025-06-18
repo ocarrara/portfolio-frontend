24afaa3e548882b53b794c8a0402a6a22d6f49ec
ocarrara <ocarrara@sorint.com>
2025-06-18 16:09:53 +0200

    change postgres hostname

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendBaseUrl: 'http://ww8ckowcks4k080kk804k0wg.158.180.235.156.sslip.io:8081/api'
+  backendBaseUrl: 'http://ww8ckowcks4k080kk804k0wg.158.180.235.156.sslip.io/api'
 };
