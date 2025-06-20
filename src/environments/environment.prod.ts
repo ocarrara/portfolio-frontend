757280eb3c415ec59c71abc1042761d15efb9925
ocarrara <ocarrara@sorint.com>
2025-06-20 08:35:45 +0200

    fix on target url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendBaseUrl: 'http://ww8ckowcks4k080kk804k0wg.158.180.235.156.sslip.io/api'
+  backendBaseUrl: 'http://nwogkkwsc0sgkc0scoskgccc.158.180.235.156.sslip.io/api'
 };
